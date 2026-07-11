6d70ce6a80a06dedbe2d0da0d63babe4991e2f2b
don <dono@MACBOOKM1PRO.local.lan>
2026-07-11 20:54:59 +0400

    Update temperature step to 0.5

--- components/samsung_ac/samsung_ac_device.cpp
+++ components/samsung_ac/samsung_ac_device.cpp
@@ -14,7 +14,7 @@ climate::ClimateTraits Samsung_AC_Climate::traits() {
 
   traits.add_feature_flags(climate::CLIMATE_SUPPORTS_CURRENT_TEMPERATURE);
 
-  traits.set_visual_temperature_step(1);
+  traits.set_visual_temperature_step(0.5);
   traits.set_visual_min_temperature(16);
   traits.set_visual_max_temperature(30);
 
