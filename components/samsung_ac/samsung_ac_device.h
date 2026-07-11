c535093da92e6e1437d6c55782ff7e13998f27b1
don <dono@MACBOOKM1PRO.local.lan>
2026-07-11 20:18:10 +0400

    Fix: direct access to map_auto_to_heat_cool_ variable

--- components/samsung_ac/samsung_ac_device.h
+++ components/samsung_ac/samsung_ac_device.h
@@ -561,8 +561,7 @@ class Samsung_AC_Device {
 
     if (_cur_power.value()) {
       if (_cur_mode.value() == Mode::Auto) {
-        climate->mode =
-            this->get_map_auto_to_heat_cool() ? climate::CLIMATE_MODE_HEAT_COOL : climate::CLIMATE_MODE_AUTO;
+        climate->mode = this->map_auto_to_heat_cool_ ? climate::CLIMATE_MODE_HEAT_COOL : climate::CLIMATE_MODE_AUTO;
       } else {
         auto opt = mode_to_climatemode(_cur_mode.value());
         if (opt.has_value()) climate->mode = opt.value();
